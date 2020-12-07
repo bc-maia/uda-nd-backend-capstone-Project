676d324858a7888db1f70fbdfcc20b7ec5f3e44a
bc-maia <bruno_engineer@hotmail.com>
2020-12-07 02:12:26 -0300

    update - using setup.sh for env. variables

--- database.py
+++ database.py
@@ -3,13 +3,6 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 
 database_path = os.environ["DATABASE_URL"]
-"""
-TODO: uncomment to test locally
-database_path = "postgres://postgres:passwd123@localhost:5432/dungeon"
-also:
-create database dungeon on postgresql database
-    and uncomment db.create_all()
-"""
 
 db = SQLAlchemy()
 
@@ -25,4 +18,4 @@ def setup_db(app):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    # db.create_all()
+    # db.create_all()   # TODO: uncomment to create local database:
---
+++ setup.sh
@@ -0,0 +1,4 @@
+export DATABASE_URL="postgres://postgres:passwd123@localhost:5432/dungeon"
+export AUTH0_DOMAIN = "uda-cafe.us.auth0.com"
+export ALGORITHMS = ["RS256"]
+export API_AUDIENCE = "https://dungeon-monster.herokuapp.com"
